29d851014c250bf7040366033cd4e1f2b4e4b760
Marco Mascorro <marco.mascorroa@icloud.com>
2023-08-19 23:25:21 -0500

    init commit

---
+++ eliat/eliat/eliatApp.swift
@@ -0,0 +1,17 @@
+//
+//  eliatApp.swift
+//  eliat
+//
+//  Created by Marco Mascorro on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct eliatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
